c8fe0f3ab634ef2374845f31cc5b6d6493556253
Dirk Arnez <smalldirkalex@gmail.com>
2024-09-25 10:01:01 +0800

    - add index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+class Counter extends EventTarget {
+  constructor(initialValue = 0) {
+    super();
+    this.value = initialValue;
+  }
+
+  #emitChangeEvent() {
+    this.dispatchEvent(new CustomEvent("valuechange", { detail: this.value }));
+  }
+
+  increment() {
+    this.value++;
+    this.#emitChangeEvent();
+  }
+
+  decrement() {
+    this.value--;
+    this.#emitChangeEvent();
+  }
+}
+
+const initialValue = 0;
+const counter = new Counter(initialValue);
+document.querySelector("#currentValue").innerText = initialValue;
+
+counter.addEventListener("valuechange", (event) => {
+  document.querySelector("#currentValue").innerText = event.detail;
+});
+
+document.querySelector("#inc").addEventListener("click", () => {
+  counter.increment();
+});
+
+document.querySelector("#dec").addEventListener("click", () => {
+  counter.decrement();
+});
